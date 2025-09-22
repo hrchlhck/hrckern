4a349c68574ae95017c38cf0b604b562dd7c25a3
Pedro Horchulhack <17484454+hrchlhck@users.noreply.github.com>
2025-09-22 17:52:09 -0300

    Add long mode bootloader

---
+++ bootloader/longmode.asm
@@ -0,0 +1,24 @@
+global long_mode_start
+extern kmain
+
+section .text
+bits 64
+
+okay:
+    ; print `OKAY` to screen
+    mov rax, 0x2f592f412f4b2f4f
+    mov qword [0xb8000], rax
+    hlt
+
+long_mode_start:
+    mov ax, 0
+    mov ss, ax
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    call kmain
+
+    call okay
+    
