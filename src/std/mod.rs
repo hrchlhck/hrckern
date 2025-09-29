22ebe23bfd32024fbdb33b6ee668ab2ba5ff6c4e
Pedro Horchulhack <17484454+hrchlhck@users.noreply.github.com>
2025-09-29 14:08:57 -0300

    Add lazy static to VGA

--- src/std/mod.rs
+++ src/std/mod.rs
@@ -1,22 +1,28 @@
 use crate::tty::color::Color;
-use crate::tty::vga::VGA;
+use crate::tty::vga::{VGA, Ptr};
+
+use lazy_static::lazy_static;
+use spin::Mutex;
 
 const DEFAULT_BG_COLOR: Color = Color::Blue;
 const DEFAULT_FG_COLOR: Color = Color::White;
 const MAX_SCREEN_WIDTH: u8 = 80;
 const MAX_SCREEN_HEIGHT: u8 = 25;
 
-pub static mut STDOUT: VGA = VGA { 
-    buffer: 0xB8000 as *mut u8,
-    row_pos: 0, 
-    col_pos: 0, 
-    cursor: 0,
-    max_rows: MAX_SCREEN_HEIGHT as u16, 
-    max_cols: MAX_SCREEN_WIDTH as u16,
-    fg: DEFAULT_FG_COLOR,
-    bg: DEFAULT_BG_COLOR,
-    default_fg: DEFAULT_FG_COLOR,
-    default_bg: DEFAULT_BG_COLOR,
-};
+
+lazy_static!(
+    pub static ref stdout: Mutex<VGA> = Mutex::new(VGA {
+        buffer: Ptr(0xb8000 as *mut u8),
+        row_pos: 0, 
+        col_pos: 0, 
+        cursor: 0,
+        max_rows: MAX_SCREEN_HEIGHT as u16, 
+        max_cols: MAX_SCREEN_WIDTH as u16,
+        fg: DEFAULT_FG_COLOR,
+        bg: DEFAULT_BG_COLOR,
+        default_fg: DEFAULT_FG_COLOR,
+        default_bg: DEFAULT_BG_COLOR,
+    });
+);
 
 pub mod lib;
