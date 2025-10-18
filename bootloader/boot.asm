88f3867e7867cb17a5fe8168ddd26a5dce5b3aaf
Pedro Horchulhack <17484454+hrchlhck@users.noreply.github.com>
2025-10-17 21:10:26 -0300

    Moved code to boot.asm

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -4,6 +4,21 @@ global start
 global magic
 extern long_mode_start
 
+section .multiboot_header
+header_start:
+    dd 0xe85250d6                
+    dd 0                         
+    dd header_end - header_start 
+    
+    ; Checksum
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+
+    dw 0    ; type
+    dw 0    ; flags
+    dd 8    ; size
+
+header_end:
+
 section .text
 bits 32
 
